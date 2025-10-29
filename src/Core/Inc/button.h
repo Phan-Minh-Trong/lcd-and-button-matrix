fc004c13a46111e7d7fd104ede8ae5b7eb0f9168
Trong Phan <127284415+FanMinkJonk@users.noreply.github.com>
2025-10-30 00:28:57 +0700

    Developing button matrix driver

--- src/Core/Inc/button.h
+++ src/Core/Inc/button.h
@@ -6,16 +6,55 @@
 #include "main.h"
 
 // Macros
+#define BUTTON_PRESSED  1 // Button is pressed
+#define BUTTON_RELEASED 0 // Button is released
+#define TOTAL_BUTTONS 16  // Total buttons count
+#define SPI_RECEIVE_BUFFER_SIZE 2 // Total bytes receive from IC 74HC165
 
+#define BUTTON_0     0
+#define BUTTON_1     1
+#define BUTTON_2     2
+#define BUTTON_3     3
+#define BUTTON_4     4
+#define BUTTON_5     5
+#define BUTTON_6     6
+#define BUTTON_7     7
+#define BUTTON_8     8
+#define BUTTON_9     9
+#define BUTTON_10   10
+#define BUTTON_11   11
+#define BUTTON_12   12
+#define BUTTON_13   13
+#define BUTTON_14   14
+#define BUTTON_15   15
 
+typedef struct Button{
+    uint8_t buttons_state[TOTAL_BUTTONS];
+} Button;
 
+/**
+ * @brief
+ * Initialize button module (must be call before using).
+ * @param hspi: Pointer to SPI handle
+ * @return 0 on success, -1 on errors
+ */
+int8_t button_init(SPI_HandleTypeDef* hspi);
 
+/**
+ * @brief
+ * Get all the buttons state (BUTTON_PRESSED or BUTTON_RELEASED)
+ * @param None
+ * @return All button states
+ */
+Button get_buttons_state(void);
 
-
-
-
-
-
+/**
+ * @brief
+ * This function updates all buttons state, the main core of the button module relies on this function (this function should be called in an interrupt function).
+ * @param None
+ * @return None
+ */
+void update_buttons_state(void);
 
 
 #endif // __BUTTON_H__
--- src/Core/Src/button.c
+++ src/Core/Src/button.c
@@ -1,1 +1,41 @@
 #include "button.h"
+
+// Variables 
+static SPI_HandleTypeDef* hspi_button = NULL; // SPI handle for button module
+
+static int8_t spi_receive_buffer[SPI_RECEIVE_BUFFER_SIZE] = {
+    0x00,
+    0x00
+};
+
+static Button buttons;
+
+// Private Functions Definition
+// TODO: Implement debounce function
+
+// Global Functions Defintion
+int8_t button_init(SPI_HandleTypeDef* hspi){
+    if(hspi == NULL){
+        return -1;
+    }
+
+    for(int i = 0; i<TOTAL_BUTTONS; ++i){
+        buttons.buttons_state[i] = BUTTON_RELEASED;
+    }
+
+    hspi_button = hspi;
+}
+
+Button get_buttons_state(void){
+    return buttons;
+}
+
+void update_buttons_state(void){
+    HAL_GPIO_WritePin(BTN_LOAD_GPIO_Port, BTN_LOAD_Pin, GPIO_PIN_RESET);
+    HAL_SPI_Receive(hspi_button, (uint8_t*)spi_receive_buffer, SPI_RECEIVE_BUFFER_SIZE, HAL_MAX_DELAY);
+    HAL_GPIO_WritePin(BTN_LOAD_GPIO_Port, BTN_LOAD_Pin, GPIO_PIN_SET);
+
+    // Debounce process....
+
+    // Update states....
+}
